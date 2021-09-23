7d0698ee465eccfc2924628553c2f1047ecdfb75
Prince Wilson <maxcell.wilson@gmail.com>
2021-09-22 21:24:36 -0400

    Update my about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,9 @@ function About(props) {
         or building and securing technology to make people's lives better.
         </p>
       <p className="text-lg pt-2">
-        I work as a software engineer at <a className="text-lg text-purple-700 underline" href="https://newsela.com">Newsela</a>. I previously worked as a software engineer over at <a target="_blank" rel="noopener noreferrer" href="https://flatironschool.com/">Flatiron School</a>, powered by <a className="text-lg text-purple-700 underline" href="https://wework.com/">WeWork</a>.
+        I am a Software Engineer on the Developer Experience team for  <a className="text-lg text-purple-700 underline" href="https://netlify.com">Netlify</a>.
+        Previously I helped build components for the Design Systems at <a className="text-lg text-purple-700 underline" href="https://newsela.com">Newsela</a>. And was a software engineer over at <a target="_blank" rel="noopener noreferrer" href="https://flatironschool.com/">Flatiron School</a>, powered by <a className="text-lg text-purple-700 underline" href="https://wework.com/">WeWork</a>.
           I started off as an instructor there and helped over 400 folks transition into a career of technology!
-          Some of my previous work includes working at Clarifai and State Farm Insurance.
         </p>
       <p className="text-lg pt-2">
         I graduated with a Bachelors degree in Computer Science with a focus in security over at the University of Central Florida. Back in school, I founded our tech club with a bunch of my friends,
