8a5b0c3c39ceb2c6ec490de10841ec533e0bb501
Prince Wilson <maxcell.wilson@gmail.com>
2021-09-26 10:57:26 -0400

    Adds more engagements

--- data/engagements.js
+++ data/engagements.js
@@ -1,23 +1,56 @@
+const types = {
+    TALK: "Talk",
+    PODCAST: "Podcast",
+    PANEL: "Panel",
+    STREAM: "Stream"
+}
+
 const engagements = [
+    // Remember to search for Things Worth Learning
     {
-        "type": "Talk",
-        "title": "Personal Sites as Personal Playgrounds",
-        "url": "https://egghead.io/lessons/egghead-personal-site-playgrounds-with-mdx"
+        type: types.STREAM,
+        title: "Real Time Notifications with the Twitch API on Learn With Jason",
+        url: "https://www.learnwithjason.dev/real-time-notifications-with-the-twitch-api"
     },
     {
-        "type": "Talk",
-        "title": "Building Custom Components with MDX",
-        "url": "https://youtu.be/-xusVshCZ3M"
+        type: types.STREAM,
+        title: "What is Rust? on Learn With Jason",
+        url: "https://www.learnwithjason.dev/what-is-rust"
     },
     {
-        "type": "Podcast",
-        "title": "Mentorship, degrees, and maintaining human connections in virtual spaces",
-        "url": "https://party-corgi-podcast.simplecast.com/episodes/maintaining-human-connections-in-virtual-spaces-with-prince-wilson"
+        type: types.PODCAST,
+        title: "Teaching Others to Code and Lessons Learned",
+        url: "https://anchor.fm/purrfect-dev/episodes/1-19---Teaching-Others-to-Code-Lessons-Learned-eva9o4/a-a5adml1"
     },
     {
-        "type": "Panel",
-        "title": "Learning GraphQL Panel",
-        "url": "https://www.youtube.com/watch?v=vsjz4E1zwgg&list=PLpi1lPB6opQxkOv1H0j4JHhHx9bydEk8H&index=5&t=8s"
+        type: types.STREAM,
+        title: "Discord Bot with Rust on Colbyashi Maru",
+        url: "https://www.youtube.com/watch?v=03sHMLdQQXk"
+    },
+    {
+        type: types.PODCAST,
+        title: "Streaming, Rust, and Bias",
+        url: "https://podcasts.apple.com/us/podcast/5-prince-wilson/id1537328655?i=1000522267142" 
+    },
+    {
+        type: types.TALK,
+        title: "Personal Sites as Personal Playgrounds",
+        url: "https://egghead.io/lessons/egghead-personal-site-playgrounds-with-mdx"
+    },
+    {
+        type: types.TALK,
+        title: "Building Custom Components with MDX",
+        url: "https://youtu.be/-xusVshCZ3M"
+    },
+    {
+        type: types.PODCAST,
+        title: "Mentorship, degrees, and maintaining human connections in virtual spaces",
+        url: "https://party-corgi-podcast.simplecast.com/episodes/maintaining-human-connections-in-virtual-spaces-with-prince-wilson"
+    },
+    {
+        type: types.PANEL,
+        title: "Learning GraphQL Panel",
+        url: "https://www.youtube.com/watch?v=vsjz4E1zwgg&list=PLpi1lPB6opQxkOv1H0j4JHhHx9bydEk8H&index=5&t=8s"
     }
 ]
 export default engagements
