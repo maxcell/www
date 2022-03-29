ffbc37036ce6153e57a649efc4d88daf8ffc0d14
Prince Wilson <maxcell.wilson@gmail.com>
2022-03-28 21:29:14 -0400

    feat: Add Blender portfolio

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -1,39 +1,46 @@
 /** @jsx h */
-import { h, Fragment } from 'preact';
-import { Helmet } from 'react-helmet';
-import { MDXProvider } from '@mdx-js/preact';
-import getShareImage from '@jlengstorf/get-share-image';
-import Callout from './components/Callout.js' // Remember local files requires extensions
-import Footer from './components/Footer.js'
+import { h, Fragment } from "preact";
+import { Helmet } from "react-helmet";
+import { MDXProvider } from "@mdx-js/preact";
+import getShareImage from "@jlengstorf/get-share-image";
+import Callout from "./components/Callout.js"; // Remember local files requires extensions
+import Footer from "./components/Footer.js";
 
 const components = {
   Callout,
-  inlineCode: props => <code className="bg-gray-100" {...props} />,
-  blockquote: props => <blockquote className="w-4/5 border-purple-500 border-l-4 pb-2 px-4 mx-auto my-4" {...props} />,
-  ul: props => <ul className="list-disc list-inside mt-2 ml-4" {...props} />,
-  'li.ul': props => <ul className="list-disc list-inside ml-5" {...props} />,
-  ol: props => <ol className="list-decimal list-inside mt-2 ml-4" {...props} />,
-  'li.ol': props => <ol className="list-disc list-inside ml-5" {...props} />,
-  a: props => <a className="text-lg text-purple-700 underline" {...props} />,
-  p: props => <p className="text-lg pt-4" {...props} />,
-  h1: props => <h1 className="font-extrabold text-4xl mt-8" {...props} />,
-  h2: props => <h2 className="font-extrabold text-3xl mt-6" {...props} />,
-  h3: props => <h3 className="font-extrabold text-2xl mt-4" {...props} />,
-  h4: props => <h4 className="font-extrabold text-xl mt-4" {...props} />,
-  h5: props => <h5 className="font-extrabold text-lg mt-4" {...props} />,
+  inlineCode: (props) => <code className="bg-gray-100" {...props} />,
+  blockquote: (props) => (
+    <blockquote
+      className="w-4/5 border-purple-500 border-l-4 pb-2 px-4 mx-auto my-4"
+      {...props}
+    />
+  ),
+  ul: (props) => <ul className="list-disc list-inside mt-2 ml-4" {...props} />,
+  "li.ul": (props) => <ul className="list-disc list-inside ml-5" {...props} />,
+  ol: (props) => (
+    <ol className="list-decimal list-inside mt-2 ml-4" {...props} />
+  ),
+  "li.ol": (props) => <ol className="list-disc list-inside ml-5" {...props} />,
+  a: (props) => <a className="text-lg text-purple-700 underline" {...props} />,
+  p: (props) => <p className="text-lg pt-4" {...props} />,
+  h1: (props) => <h1 className="font-extrabold text-4xl mt-8" {...props} />,
+  h2: (props) => <h2 className="font-extrabold text-3xl mt-6" {...props} />,
+  h3: (props) => <h3 className="font-extrabold text-2xl mt-4" {...props} />,
+  h4: (props) => <h4 className="font-extrabold text-xl mt-4" {...props} />,
+  h5: (props) => <h5 className="font-extrabold text-lg mt-4" {...props} />,
   wrapper: (props) => {
-    const figureItOut = getShareImage.default || getShareImage
+    const figureItOut = getShareImage.default || getShareImage;
     const socialImage = figureItOut({
       title: props.title,
-      titleExtraConfig: '_bold',
-      tagline: '',
-      cloudName: 'maxcell',
-      imagePublicID: 'prince_social_template',
-      titleFont: 'roboto',
-      textColor: '222426',
+      titleExtraConfig: "_bold",
+      tagline: "",
+      cloudName: "maxcell",
+      imagePublicID: "prince_social_template",
+      titleFont: "roboto",
+      textColor: "222426",
       textAreaWidth: 616,
       textLeftOffset: 624,
-    })
+    });
 
     return (
       <Fragment>
@@ -42,26 +49,30 @@ const components = {
           <meta name="twitter:image" content={socialImage} />
         </Helmet>
         <article className="prose max-w-none">
-          {props.title ? <h2 className="font-extrabold text-3xl mt-6">{props.title}</h2> : null}
+          {props.title ? (
+            <h2 className="font-extrabold text-3xl mt-6">{props.title}</h2>
+          ) : null}
           {props.children}
         </article>
       </Fragment>
-    )
+    );
   },
-  codeblock: props => (
+  codeblock: (props) => (
     <div
       className="my-4 rounded shadow overflow-auto"
       style={{ backgroundColor: "#011627" }}
       dangerouslySetInnerHTML={{ __html: props.children }}
     />
   ),
-}
-
+};
 
 function NavLink(props) {
   return (
-    <a {...props} className={`mr-6 text-2xl focus:underline hover:underline ${props.className}`} />
-  )
+    <a
+      {...props}
+      className={`mr-6 text-2xl focus:underline hover:underline ${props.className}`}
+    />
+  );
 }
 
 // Interesting that you can't use <></>
@@ -69,15 +80,20 @@ function NavLink(props) {
 // Note that most modern transpilers allow you to use a shorter syntax for Fragments.
 // https://preactjs.com/guide/v10/components#fragments
 export default function PageWrapper(props) {
-  const title = props.title || "Prince Wilson - Developer"
-  const description = props.description || "An organically growing notebook of thoughts and learnings!"
+  const title = props.title || "Prince Wilson - Developer";
+  const description =
+    props.description ||
+    "An organically growing notebook of thoughts and learnings!";
   return (
     <Fragment>
       <Helmet>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/style.css" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
+          rel="stylesheet"
+        />
         <style>
           {`
             .mdx-highlight-line {
@@ -88,14 +104,19 @@ export default function PageWrapper(props) {
             body {
               font-family: IBM Plex Sans;
             }
-          `
-          }
+          `}
         </style>
         <meta charSet="utf-8" />
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="og:image" content="https://res.cloudinary.com/maxcell/image/upload/v1579584116/main_social.png" />
-        <meta name="twitter:image" content="https://res.cloudinary.com/maxcell/image/upload/v1579584116/main_social.png" />
+        <meta
+          name="og:image"
+          content="https://res.cloudinary.com/maxcell/image/upload/v1579584116/main_social.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/maxcell/image/upload/v1579584116/main_social.png"
+        />
         <meta name="og:title" content={title} />
         <meta name="twitter:title" content={props.title} />
         <meta name="og:type" content="website" />
@@ -107,21 +128,34 @@ export default function PageWrapper(props) {
       </Helmet>
       <div className="container max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col min-h-screen">
         <nav className="mt-10 mb-4 flex justify-between">
-          <NavLink href="/" className="font-bold">Prince</NavLink>
+          <NavLink href="/" className="font-bold">
+            Prince
+          </NavLink>
           <ul className="flex">
-            <li><NavLink className="font-bold" href="/about">About</NavLink></li>
-            <li><NavLink className="font-bold" href="/garden">Blog</NavLink></li>
+            <li>
+              <NavLink className="font-bold" href="/about">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="font-bold" href="/garden">
+                Blog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="font-bold" href="/blender">
+                Blender
+              </NavLink>
+            </li>
           </ul>
         </nav>
         {/* Does MDXProvider only render on MDX Pages */}
         <MDXProvider components={components}>
-          <main>
-            {props.children}
-          </main>
+          <main>{props.children}</main>
         </MDXProvider>
         <hr className="mt-10 mb-4 divide-y-4 border-purple-500 divide-dashed" />
         <Footer />
       </div>
     </Fragment>
-  )
+  );
 }
