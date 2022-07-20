29302f579769531427d3dbac107fc01cbc459ab4
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-20 19:54:03 -0400

    Use the extension logic

--- postcss-config/tailwind.config.js
+++ postcss-config/tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ['./src/**/*.js'],
     theme: {
         extend: {
             fontFamily: {
-                sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans]
+                sans: [
+                    '"IBM Plex Sans"',
+                    ...defaultTheme.fontFamily.sans
+                ]
             }
         },
     },
