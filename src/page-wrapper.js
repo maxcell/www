16bee4d820da6d332166bc03cdd2193b7adb957d
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-20 20:23:35 -0400

    Remove unusued configurations (#15)

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -90,12 +90,13 @@ export default function PageWrapper(props) {
     <Fragment>
       <Helmet>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/style.css" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
           rel="stylesheet"
         />
+        <link rel="stylesheet" href="/style.css" />
         <style>
           {`
             .mdx-highlight-line {
