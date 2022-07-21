4657e6df0f8cc67b918d148066f29904f134874d
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-20 20:21:43 -0400

    Adds crossorigin

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -90,7 +90,8 @@ export default function PageWrapper(props) {
     <Fragment>
       <Helmet>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
           rel="stylesheet"
